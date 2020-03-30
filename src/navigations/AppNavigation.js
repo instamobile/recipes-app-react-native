ed44a2943bc7c5f9f98d50fd7cf897c8ff52878d
Ezichi Chukwudi <chukwudiezichi@gmail.com>
2020-03-30 23:52:01 +0100

    upgrade rnav to latest  of v4

--- src/navigations/AppNavigation.js
+++ src/navigations/AppNavigation.js
@@ -1,4 +1,10 @@
-import { createDrawerNavigator, createStackNavigator, createAppContainer } from 'react-navigation';
+import { createAppContainer } from 'react-navigation';
+import {createDrawerNavigator} from 'react-navigation-drawer'
+import {createStackNavigator} from 'react-navigation-stack'
+/* import React from 'react'
+import {createStackNavigator} from '@react-navigation/stack'
+import {NavigationContainer} from '@react-navigation/native'
+import {createDrawerNavigator} from '@react-navigation/drawer' */
 import HomeScreen from '../screens/Home/HomeScreen';
 import CategoriesScreen from '../screens/Categories/CategoriesScreen';
 import RecipeScreen from '../screens/Recipe/RecipeScreen';
@@ -7,6 +13,31 @@ import DrawerContainer from '../screens/DrawerContainer/DrawerContainer';
 import IngredientScreen from '../screens/Ingredient/IngredientScreen';
 import SearchScreen from '../screens/Search/SearchScreen';
 import IngredientsDetailsScreen from '../screens/IngredientsDetails/IngredientsDetailsScreen';
+
+/* const Stack = createStackNavigator();
+
+function MainNavigator() {
+  return(
+    <Stack.Navigator
+      screenOptions={{
+          headerTitleStyle: {
+            fontWeight: 'bold',
+            textAlign: 'center',
+            alignSelf: 'center',
+            flex: 1,
+          }
+      }}
+    >
+      <Stack.Screen name='Home' component={HomeScreen} />
+      <Stack.Screen name='Categories' component={CategoriesScreen}/>
+      <Stack.Screen name='Recipe' component={RecipeScreen}/>
+      <Stack.Screen name='RecipesList' component={RecipesListScreen} />
+      <Stack.Screen name='Ingredient' component={IngredientScreen} />
+      <Stack.Screen name='Search' component={SearchScreen} />
+      <Stack.Screen name='IngredientsDetails' component={IngredientsDetailsScreen} />
+    </Stack.Navigator>
+  )
+} */
 
 const MainNavigator = createStackNavigator(
   {
@@ -30,7 +61,24 @@ const MainNavigator = createStackNavigator(
       }
     })
   }
-);
+); 
+
+/* const Drawer = createDrawerNavigator();
+
+function DrawerStack() {
+  return(
+    <Drawer.Navigator
+      drawerPosition='left'
+      initialRouteName='Main'
+      drawerStyle={{
+        width: 250
+      }}
+      drawerContent={props=> DrawerContainer}
+    >
+      <Drawer.Screen name='Main' component={MainNavigator} />
+    </Drawer.Navigator>
+  )
+} */
 
 const DrawerStack = createDrawerNavigator(
   {
@@ -44,6 +92,14 @@ const DrawerStack = createDrawerNavigator(
   }
 );
 
+/* export default function AppContainer() {
+  return(
+    <NavigationContainer>
+      <DrawerStack/>
+    </NavigationContainer>
+  )
+} */
+ 
 export default AppContainer = createAppContainer(DrawerStack);
 
 console.disableYellowBox = true;
