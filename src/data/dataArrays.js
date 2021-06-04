e11ed846379a94ee7068f5c838ac99a937a5cc5a
KaushikKondu <ronykaushik2000@gmail.com>
2021-06-04 18:22:48 +0530

    ,

--- src/data/dataArrays.js
+++ src/data/dataArrays.js
@@ -39,16 +39,6 @@ export const categories = [
   },
 ];
 
-export const videos=[
-    {
-      videoId:"EiVoWp5b93s"
-    },
-    {
-      
-      videoId:"xkpHye6H_E"
-    }
-  
-];
 
 export const recipes = [
   {
@@ -67,10 +57,9 @@ export const recipes = [
     description:
       '-- Start with cleaned and peeled russet potatoes that you have cut into 3/8-inch match sticks. Place in bowl of very cold water: keep rinsing and changing the water until the water is clear; drain thoroughly and dry with paper towels or a clean lint-free kitchen towel.\n\n -- Meanwhile, you preheat your hot oil to 350 degrees F. Place prepared taters in oil and cook about 5 minutes. They will have that blond-tone color to them. \n\n -- Note: Once you add cold potatoes to the hot oil, the temperature of your oil is going to drop - you want it to be somewhere between 330 - 325 degrees F. \n\n -- Remove from oil; drain and cool. Now - either refrigerate until ready to finish cooking, or cool completely and freeze up to 3 months. To freeze properly - place completely cooled fries in single layer on tray and place in freezer until frozen. Then bag them.\n\n -- To finish cooking - preheat your oil to 400* F. Add your cold fries (which will drop the oil temp - which is fine because you want it near the 375 degrees F. temp) and cook a few minutes until done. Lightly salt them and shake well so that the salt distributes well and they are not salty.',
       videoSeries : [
-        "DC471a9qrU4",
-        "tVCYa_bnITg",
-        "K74l26pE4YA",
-        "m3OjWNFREJo",
+        "EhyNJdL4w-E",
+        "naE27iCpVVg",
+        "pXFPtxNMEOM",
       ]
   },
   {
@@ -93,10 +82,8 @@ export const recipes = [
     ],
     description: '-- In a blender, combine all ingredients and blend until smooth. Then divide between 2 cups and top with blackberries, if desired.',
     videoSeries : [
-      "DC471a9qrU4",
-      "tVCYa_bnITg",
-      "K74l26pE4YA",
-      "m3OjWNFREJo",
+      "W5LaGxkn4l4",
+      "Gmt-8zdlNoM",
     ]
   },
   {
@@ -126,10 +113,9 @@ export const recipes = [
     description:
       '-- Beat the egg and then combine it with water in a bowl. Stir. Combine the flour, salt, MSG, pepper, onion powder and garlic powder in a gallon size zip lock bag. Pound each of the breast filets until about 1/4-inch thick. Then cut into bite sized pieces. Coat each piece with the flour mixture by shaking in the zip lock bag. Remove and coat in the egg mixture. Then coat in the flour mixture again. Shake to coat. Deep fry at 375 degrees for 10-12 minutes, until browned and crispy.',
       videoSeries : [
-        "DC471a9qrU4",
-        "tVCYa_bnITg",
-        "K74l26pE4YA",
-        "m3OjWNFREJo",
+        "j7rgSxPcyVc",
+        "mwiKZ4qmRnQ",
+        "BJ6lcH2htYY",
       ]
   },
   {
@@ -162,10 +148,9 @@ export const recipes = [
     description:
       '-- In a medium pot over medium heat, heat 1 tablespoon oil. Add onion and cook until soft, 5 minutes. Add garlic and cook until fragrant, 1 minute more. Add tomato paste and stir to coat onion and garlic. Add ground beef and cook, breaking up meat with a wooden spoon, until no longer pink, 6 minutes. Drain fat.\n\n -- Return beef to pot and season with chili powder, paprika, salt, and pepper. Add tomato sauce and kidney beans. Bring to a boil, then reduce heat and let simmer 15 minutes. Add some chili to center of each tortilla, leaving room to fold in edges. Top with Fritos, then cheddar. Fold edges of tortillas toward the center, creating pleats. Invert Crunchwraps so pleats are on the bottom and stay together.\n\n -- In medium skillet over medium heat, heat remaining tablespoon oil. Add a Crunchwrap seam side down and cook until tortilla is golden, 3 to 5 minutes per side. Repeat with remaining Crunchwraps',
       videoSeries : [
-        "DC471a9qrU4",
-        "tVCYa_bnITg",
-        "K74l26pE4YA",
-        "m3OjWNFREJo",
+        "CfB-jApRuvg",
+        "zOeefB9Bmag",
+        "ceqxA1U64ns",
       ]
   },
   {
@@ -200,10 +185,10 @@ export const recipes = [
     description:
       '-- Preheat fryer to 350°F. Thoroughly mix together all spices. Combine spices with flour, brown sugar and salt. Dip chicken pieces in egg white to lightly coat them, then transfer to flour mixture. Turn a few times and make sure the flour mix is really stuck to the chicken.\n\n -- Repeat with all the chicken pieces. Let chicken pieces rest for 5 minutes so crust has a chance to dry a bit. Fry chicken in batches. Breasts and wings should take 12-14 minutes, and legs and thighs will need a few more minutes. Chicken pieces are done when a meat thermometer inserted into the thickest part reads 165°F. Let chicken drain on a few paper towels when it comes out of the fryer. Serve hot.',
       videoSeries : [
-        "DC471a9qrU4",
-        "tVCYa_bnITg",
-        "K74l26pE4YA",
-        "m3OjWNFREJo",
+        "7c1U7REkWaw",
+        "3nQTlGMO9NY",
+        "Eb0VzmWU2VI",
+        "Z1OLG7F3HD4",
       ]
   },
   {
@@ -233,10 +218,10 @@ export const recipes = [
     description:
       '-- In a medium shallow bowl, whisk together olive oil, lime juice, paprika, chili powder, cumin, and cayenne. Add cod, tossing until evenly coated. Let marinate 15 minutes. Meanwhile, make slaw: In a large bowl, whisk together mayonnaise, lime juice, cilantro, and honey. Stir in cabbage, corn, and jalapeño. Season with salt and pepper.\n\n -- In a large nonstick skillet over medium-high heat, heat vegetable oil. Remove cod from marinade and season both sides of each filet with salt and pepper. Add fish flesh side-down. Cook until opaque and cooked through, 3 to 5 minutes per side.\n\n -- Let rest 5 minutes before flaking with a fork. Assemble tacos: Serve fish over grilled tortillas with corn slaw and avocado. Squeeze lime juice on top and garnish with sour cream. ',
       videoSeries : [
-        "DC471a9qrU4",
-        "tVCYa_bnITg",
-        "K74l26pE4YA",
-        "m3OjWNFREJo",
+        "H4JgGHpJyr4",
+        "qDFqRxeEn9o",
+        "0Lr7gRFcFxM",
+        "aeZ5QcWAjPU",
       ]
   },
   {
@@ -270,10 +255,10 @@ export const recipes = [
     description:
       '-- In a large bowl, combine 2 tablespoons oil, lemon juice and seasonings; add the chicken. Turn to coat; cover. Refrigerate for 1-4 hours In a large skillet, saute peppers and onions in remaining oil until crisp-tender. Remove and keep warm. Drain chicken, discarding marinade. In the same skillet, cook chicken over medium-high heat for 5-6 minutes or until no longer pink.\n\n -- Return pepper mixture to pan; heat through. Spoon filling down the center of tortillas; fold in half. Serve with toppings as desired.',
       videoSeries : [
-        "DC471a9qrU4",
-        "tVCYa_bnITg",
-        "K74l26pE4YA",
-        "m3OjWNFREJo",
+        "VSmCWKLHWxk",
+        "SdEcMiXdDxs",
+        "0g1bv8b2zKU",
+        "DeNsUhaUH6k",
       ]
   },
   {
@@ -307,10 +292,10 @@ export const recipes = [
     description:
       '-- Place a rack in upper third of oven. Place a large cast-iron skillet on rack and preheat oven to 500° (or as high as your oven will go). Place pizza dough in a large bowl, pour a little oil over, and turn to coat. Cover bowl with plastic and let dough proof at room temperature while pan and oven heat up.\n\n -- Meanwhile, cook hot sauce, marinara sauce, and butter in a medium saucepan over medium heat, stirring occasionally, until butter is melted. Stir in cream, reduce heat to low, and simmer, stirring occasionally, until slightly thickened and warmed through, about 10 minutes. Heat 1 Tbsp. oil in a large skillet over medium-high. Add chicken, toss to coat, then add ¼ cup Buffalo sauce.\n\n -- Cook chicken, tossing occasionally, until heated through, about 2 minutes. Reduce heat and simmer, stirring often, until chicken is well coated and sauce is slightly thickened, about 5 minutes. Meanwhile, whisk yogurt, lemon juice, celery salt, garlic powder, ¼ cup blue cheese, ½ tsp. pepper, and 2 Tbsp. water in a small bowl, adding more water if sauce seems too thick (it should be pourable); set aside.\n\n -- Turn out dough onto a lightly floured work surface. Shape with your hands into a round that’s slightly larger than the cast-iron skillet you’re using. Take hot skillet out of oven (watch that handle!) and place on a heatproof surface. Add a little flour to pan. Lay dough in skillet, then work edges of dough up sides of skillet with your fingertips (use a rubber spatula or wooden spoon if you’re nervous about touching the hot pan). Drizzle a little oil around inside edge of pan so that it trickles behind and underneath dough, which will encourage browning and help it release.\n\n -- Spread about ⅓ cup Buffalo sauce over dough. Arrange mozzarella over, then top with remaining ¼ cup blue cheese. Arrange chicken mixture on top. Bake pizza on top rack until crust and cheese are nicely browned, 15–20 minutes. Transfer skillet to stovetop (again, watch that handle!) and let pizza rest a few minutes. Using a spatula, slide pizza onto a cutting board or platter. Arrange celery over, then top with reserved blue cheese dressing. Season with pepper, then drizzle with oil.',
       videoSeries : [
-        "DC471a9qrU4",
-        "tVCYa_bnITg",
-        "K74l26pE4YA",
-        "m3OjWNFREJo",
+        "0pnsU0-kGDA",
+        "skOnx6T7FaQ",
+        "0kkzINSUF7I",
+        "mJtUOVJvh_YA",
       ]
   },
   {
@@ -336,16 +321,16 @@ export const recipes = [
     description:
       '-- Put the egg yolks into a bowl, finely grate in the Parmesan, season with pepper, then mix well with a fork and put to one side. Cut any hard skin off the pancetta and set aside, then chop the meat. Cook the spaghetti in a large pan of boiling salted water until al dente.\n\n -- Meanwhile, rub the pancetta skin, if you have any, all over the base of a medium frying pan (this will add fantastic flavour, or use 1 tablespoon of oil instead), then place over a medium-high heat. Peel the garlic, then crush with the palm of your hand, add it to the pan and leave it to flavour the fat for 1 minute. Stir in the pancetta, then cook for 4 minutes, or until it starts to crisp up. Pick out and discard the garlic from the pan, then, reserving some of the cooking water, drain and add the spaghetti.\n\n -- Toss well over the heat so it really soaks up all that lovely flavour, then remove the pan from the heat. Add a splash of the cooking water and toss well, season with pepper, then pour in the egg mixture – the pan will help to cook the egg gently, rather than scrambling it. Toss well, adding more cooking water until it’s lovely and glossy. Serve with a grating of Parmesan and an extra twist of pepper.',
       videoSeries : [
-        "DC471a9qrU4",
-        "tVCYa_bnITg",
-        "K74l26pE4YA",
-        "m3OjWNFREJo",
+        "4a0KpB5rC3U",
+        "vBcXtkXzuVE",
+        "dLbWuG7lkJs",
+        "o5M6Vsb4iTM",
       ]
   },
   {
     recipeId: 8,
     categoryId: 2,
-    title: 'Lasagne',
+    title: 'Lasagna',
     photo_url: 'https://images8.alphacoders.com/817/817353.jpg',
     photosArray: [
       'https://previews.123rf.com/images/somegirl/somegirl1509/somegirl150900048/46103208-top-view-of-a-delicious-traditional-italian-lasagna-made-with-minced-beef-bolognese-sauce-topped-wit.jpg',
@@ -375,10 +360,10 @@ export const recipes = [
     description:
       '-- Cook noodles according to package directions; drain. Meanwhile, in a Dutch oven, cook sausage, beef and onion over medium heat 8-10 minutes or until meat is no longer pink, breaking up meat into crumbles. Add garlic; cook 1 minute. Drain. Stir in tomatoes, tomato paste, water, sugar, 3 tablespoons parsley, basil, fennel, 1/2 teaspoon salt and pepper; bring to a boil. Reduce heat; simmer, uncovered, 30 minutes, stirring occasionally. In a small bowl, mix egg, ricotta cheese, and remaining parsley and salt. Preheat oven to 375°. Spread 2 cups meat sauce into an ungreased 13x9-in. baking dish. Layer with 3 noodles and a third of the ricotta mixture. Sprinkle with 1 cup mozzarella cheese and 2 tablespoons Parmesan cheese.\n\n -- Repeat layers twice. Top with remaining meat sauce and cheeses (dish will be full). Bake, covered, 25 minutes. Bake, uncovered, 25 minutes longer or until bubbly. Let stand 15 minutes before serving.',
       videoSeries : [
-        "DC471a9qrU4",
-        "tVCYa_bnITg",
-        "K74l26pE4YA",
-        "m3OjWNFREJo",
+        "U0zk2H3mMoA",
+        "1kMD9RyvGMY",
+        "DhwGdJA-WiE",
+        "J9mR85TXuic",
       ]
   },
   {
@@ -400,15 +385,15 @@ export const recipes = [
       [42, '50g'],
       [16, '3'],
       [1, '2 teaspoons'],
-      [4, '2 teaspoons']
+      [4, '2 teaspoons'],
     ],
     description:
-      '-- Put the egg yolks into a bowl, finely grate in the Parmesan, season with pepper, then mix well with a fork and put to one side. Cut any hard skin off the pancetta and set aside, then chop the meat. Cook the spaghetti in a large pan of boiling salted water until al dente.\n\n -- Meanwhile, rub the pancetta skin, if you have any, all over the base of a medium frying pan (this will add fantastic flavour, or use 1 tablespoon of oil instead), then place over a medium-high heat. Peel the garlic, then crush with the palm of your hand, add it to the pan and leave it to flavour the fat for 1 minute. Stir in the pancetta, then cook for 4 minutes, or until it starts to crisp up. Pick out and discard the garlic from the pan, then, reserving some of the cooking water, drain and add the spaghetti.\n\n -- Toss well over the heat so it really soaks up all that lovely flavour, then remove the pan from the heat. Add a splash of the cooking water and toss well, season with pepper, then pour in the egg mixture – the pan will help to cook the egg gently, rather than scrambling it. Toss well, adding more cooking water until it’s lovely and glossy. Serve with a grating of Parmesan and an extra twist of pepper.',
+      'In a large skillet, in 2 tablespoons vegetable oil (or ghee) fry potatoes until brown, drain and reserve the potatoes. Add remaining 2 tablespoons oil to the skillet and fry onion, garlic and ginger until onion is soft and golden. Add chili, pepper, turmeric, cumin, salt and the tomatoes. Fry, stirring constantly for 5 minutes. Add yogurt, mint, cardamom and cinnamon stick. Cover and cook over low heat, stirring occasionally until the tomatoes are cooked to a pulp. It may be necessary to add a little hot water if the mixture becomes too dry and starts to stick to the pan. \n\n -- When the mixture is thick and smooth, add the chicken pieces and stir well to coat them with the spice mixture. Cover and cook over very low heat until the chicken is tender, approximately 35 to 45 minutes. There should only be a little very thick gravy left when chicken is finished cooking. If necessary cook uncovered for a few minutes to reduce the gravy. \n\n -- In a large skillet, heat vegetable oil (or ghee) and fry the onions until they are golden. Add saffron, cardamom, cloves, cinnamon stick, ginger and rice. Stir continuously until the rice is coated with the spices. \n\n -- In a medium-size pot, heat the chicken stock and salt. When the mixture is hot pour it over the rice and stir well. Add the chicken mixture and the potatoes; gently mix them into the rice. Bring to boil. Cover the saucepan tightly, turn heat to very low and steam for 20 minutes. Do not lift lid or stir while cooking. Spoon biryani onto a warm serving dish.',
       videoSeries : [
-        "DC471a9qrU4",
-        "tVCYa_bnITg",
-        "K74l26pE4YA",
-        "m3OjWNFREJo",
+        "95BCU1n268w",
+        "oiEH4z_EoDw",
+        "XTM0wwIGXfY",
+        "VZsgMLQUJlM",
       ]
   },
 ];
